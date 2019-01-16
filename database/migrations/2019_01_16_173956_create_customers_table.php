74e81a829e02890ff1640aabce4a7f7e4e2662b5
MKamran1994 <46578051+MKamran1994@users.noreply.github.com>
2019-01-16 10:21:25 -0800

    assignment.3

---
+++ database/migrations/2019_01_16_173956_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name' 100);
+            $table->int('cnic')->default('099999');
+            $table->boolean('registration_num');
+            $table->string('address');
+            $table->boolean('street_num');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
